9dc09235753519fc26fadf0e91fb8920734d7fc6
Daniel <daniel@selareid.com>
2022-02-09 13:22:25 +1300

    removed ServerAPI.shuffle which was used for testing before actual server fetching was added

--- src/hooks/ServerAPI.ts
+++ src/hooks/ServerAPI.ts
@@ -2,24 +2,6 @@ import { useEffect, useState } from "react";
 
 const API_URI = "https://example.com/api/";
 
-function shuffle(array: any[]) {
-    let currentIndex = array.length,  randomIndex;
-  
-    // While there remain elements to shuffle...
-    while (currentIndex != 0) {
-  
-      // Pick a remaining element...
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
-  
-      // And swap it with the current element.
-      [array[currentIndex], array[randomIndex]] = [
-        array[randomIndex], array[currentIndex]];
-    }
-  
-    return array;
-  }
-
 function useNotesList() {
     const [notesList, setNotesList] = useState<{status: Status, list: string[]}>({status: {statusCode: 200, statusText: "Initialised"}, list: []});
 
