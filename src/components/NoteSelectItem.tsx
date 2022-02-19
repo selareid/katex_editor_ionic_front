ae8b37778a709e8cc8bed22ff6a83aa3d45244a4
Daniel <daniel@selareid.com>
2022-02-19 23:20:11 +1300

    NoteSelectItem now uses it's own local state for selectedNote This is updated from the props.selectedNote via useEffect this means that onChange event has the currect state when run from props.selectedNote update

--- src/components/NoteSelectItem.tsx
+++ src/components/NoteSelectItem.tsx
@@ -5,12 +5,14 @@ import ServerAPI from "../hooks/ServerAPI";
 
 const NoteSelectItem: React.FC<{selectedNote: string | null, onChange: ((event: CustomEvent<SelectChangeEventDetail<any>>) => void), listRefreshConditions?: any[]}> = (props) => {
     const { notesList, refreshNotesList } = ServerAPI.useNotesList();
+    const [selectedNote, setSelectedNote] = useState<string | null>();
 
     useEffect(() => refreshNotesList(), (props.listRefreshConditions === undefined ? [] : props.listRefreshConditions));
+    useEffect(() => setSelectedNote(props.selectedNote), [props.selectedNote])
 
     return <IonItem>
         <IonLabel>{"Note"}</IonLabel>
-        <IonSelect value={props.selectedNote} onIonChange={props.onChange} style={{"--padding-start": "0px"}} interface="popover" > {
+        <IonSelect value={selectedNote} onIonChange={props.onChange} style={{"--padding-start": "0px"}} interface="popover" > {
                 notesList.list.map((noteName) => <IonSelectOption key={noteName} value={noteName}>{noteName}</IonSelectOption>)
             } </IonSelect>
     </IonItem>;
